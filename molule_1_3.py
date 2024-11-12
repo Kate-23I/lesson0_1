b023015560a491abe54d14bb9b8a0f05de58e962
Kate-23I <r.s.kate.231@gmail.com>
2024-11-13 01:24:15 +0300

    Add files via upload

---
+++ molule_1_3.py
@@ -0,0 +1,13 @@
+Name = 'Катя'
+print(Name, type(Name))
+Age = 33
+print(Age, type(Age))
+Age = Age + 1
+print(Age, type(Age))
+New = 2
+Age = Age + New
+print(Age, type(Age))
+Is_student = True
+print(Is_student, type(Is_student))
+All = [Name, Age, Is_student]
+print(All, type(All))
