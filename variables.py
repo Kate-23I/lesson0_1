f1758311e8f465267868fb403122c913fddd9905
Kate-23I <r.s.kate.231@gmail.com>
2024-11-18 03:05:38 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,6 @@
+namber_homeworks = 12
+namber_hours_spent = 1.5
+name_courses = 'Python,'
+time_of_one_exercise = (namber_hours_spent / float(namber_homeworks))
+print('Курс:', name_courses, 'всего задач:', str(namber_homeworks) + ',', 'затрачено часов:', str(namber_hours_spent) + ',',
+      'среднее время выполнения', time_of_one_exercise, 'часа.')
